d0faa7dc3f389d78cc98f25fe594d4814ae17ce2
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-15 20:08:34 -0300

    ajustes containers grid

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,25 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #d9e6f6;
+  }
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +29,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+const Box = styled.div`
+  background-color: #fff;
+  border-radius: 8px;
+`;
+const MainGrid = styled.main`
+  display: grid;
+  grid-gap: 10px;
+  padding: 16px;
+  @media (min-width: 860px) {
+    grid-template-areas: "profileArea wellcomeArea profileRelationsArea";
+    grid-template-columns: 160px 1fr 312px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div style={{ gridArea: "profileArea" }}>
+        <Box>imagem</Box>
+      </div>
+      <div style={{ gridArea: "wellcomeArea" }}>
+        <Box>Bem vindo </Box>
+      </div>
+      <div style={{ gridArea: "profileRelationsArea" }}>
+        <Box>Comunidade</Box>
+      </div>
+    </MainGrid>
+  );
 }
